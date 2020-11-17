7118563863f253fbbf6aae863a5a796c37bacbfb
shridhar kalagi <shridharkalagi@gmail.com>
2020-11-17 08:08:43 +0530

    update chrome libs

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4
 sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt-get -y install xvfb gtk2-engines-pixbuf
+sudo apt-get -y install xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
+# Optional but nifty: For capturing screenshots of Xvfb display:
+sudo apt-get -y install imagemagick x11-apps
 
-
-
+# Make sure that Xvfb starts everytime the box/vm is booted:
+echo "Starting X virtual framebuffer (Xvfb) in background..."
+Xvfb -ac :99 -screen 0 1280x1024x16 &
+export DISPLAY=:99
