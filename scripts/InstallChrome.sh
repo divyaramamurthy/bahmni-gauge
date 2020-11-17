0e2a886c835de8b77931083ed5c9054f33f69d9c
shridhar kalagi <shridharkalagi@gmail.com>
2020-11-17 08:24:07 +0530

    update chrome options and chrome is crashing on CI

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -12,4 +12,4 @@ sudo apt-get -y install imagemagick x11-apps
 # Make sure that Xvfb starts everytime the box/vm is booted:
 echo "Starting X virtual framebuffer (Xvfb) in background..."
 Xvfb -ac :99 -screen 0 1280x1024x16 &
-export DISPLAY=:99
+export DISPLAY=:0.0
